94eeeaba5ff4d787c37074df06d769918eeb5a90
Bruno Ferreira <bgsf1991@gmail.com>
2023-05-02 21:58:35 -0300

    Atualização do Projeto - 2

---
+++ labeshop/src/components/Cart/Cart.js
@@ -0,0 +1,19 @@
+
+const Cart = () => {
+  return (
+    <div className="cart column-1">
+        <h2>Cart Items</h2>
+        <p>Product</p>
+        <p>Product</p>
+        <p>Product</p>
+        <p>Product</p>
+        <p>Product</p>
+        <p>Product</p>
+        <p>Product</p>
+        <p>Product</p>
+        <p>Product</p>
+    </div>
+  );
+};
+
+export default Cart;
